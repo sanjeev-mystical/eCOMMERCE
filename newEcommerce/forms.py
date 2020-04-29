07944940b4a75895f5b732135c937c3c56535fd9
sanjeev-mystical <shuklasanjeev225@gmail.com>
2020-04-29 14:38:39 +0530

    some ajax changes

--- newEcommerce/forms.py
+++ newEcommerce/forms.py
@@ -9,10 +9,10 @@ class ContactForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': "form-control", "placeholder": "Your Email"}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': "form-control", "placeholder": "Your Message"}))
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if not "gmail.com" in email:
-            raise forms.ValidationError("Email has to be gmail.com")
-        return email
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     if not "gmail.com" in email:
+    #         raise forms.ValidationError("Email has to be gmail.com")
+    #     return email
 
 
